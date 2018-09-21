cf005568ab4a7384cdeb4700e5e343597f15c8ef
wesalnama111 <wesal.iintellect@gmail.com>
2018-09-21 02:20:54 -0700

    new changewS

--- js/main.js
+++ js/main.js
@@ -17,34 +17,58 @@
 			this.DOM.bgshape = document.querySelector('svg.scene .path3');
 			
 			this.initialShape = this.DOM.bgshape.getAttribute('d');
-			console.log(this.initialShape);
+			this.initialcolor = this.DOM.bgshape.getAttribute('fill');
+			this.initialclass = this.DOM.bgshape.getAttribute('class');
+
+			this.eventClass=null
 			this.initEvents();
 		}
+		// var eventClass= null;
 		changeShape(el) {
+			
+			var classes= document.getElementById('path3');
+			classes.classList.remove(this.eventClass);
+			classes.classList.add(el.dataset.class);
+			this.eventClass=null;
+			console.log("null:",this.eventClass);
+			this.eventClass=el.dataset.class;
 			anime.remove(this.DOM.bgshape);
+			anime.remove(this.initialcolor);
+			console.log("added class",this.eventClass);
 			anime({
 				targets: this.DOM.bgshape,
-				duration: 1000,
+				duration: 3000,
+				// easing: 'easeInOutBack',
 				easing: 'easeOutQuad',
-				d: el.dataset.shape
+				d: el.dataset.shape,
+				// class:el.dataset.class
+				
 			});
+			
+
 		}
+
 		resetShape() {
+			  var resetcolor= document.getElementById('path3');
+			  resetcolor.classList.remove(this.eventClass);
+			  // resetcolor.removeAttribute('d').value;
+			  
 			anime.remove(this.DOM.bgshape);
 			anime({
 				targets: this.DOM.bgshape,
-				duration: 4000,
+				duration: 3000,
 				easing: 'easeOutQuint',
-				d: this.initialShape
+				d: this.initialShape,
 			});
+			// console.log()
 		}
+
 		initEvents() {
 			this.mouseenterFn = (ev) => this.mouseTimeout = setTimeout(() => {
 				this.isActive = true;
 				this.changeShape(ev.target);
 				clearTimeout(this.resetTimeout);
 			}, 50);
-			
 			this.mouseleaveFn = () => {
 				clearTimeout(this.mouseTimeout);
 				if( !this.isActive ) return;
@@ -54,7 +78,6 @@
 					this.resetShape();
 				}, 300);
 			};
-			
 			this.DOM.items.forEach((item) => {
 				item.addEventListener('mouseenter', this.mouseenterFn);
 				item.addEventListener('mouseleave', this.mouseleaveFn);
